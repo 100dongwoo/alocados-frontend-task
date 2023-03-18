d55e03c348c0fcfcb9124eea938c0e28c196a1a3
100dongwoo <qorehddn123@naver.com>
2023-03-18 16:09:21 +0900

    feat : exchangeHistorylayout

--- src/pages/ExchangeHistoryPage/index.tsx
+++ src/pages/ExchangeHistoryPage/index.tsx
@@ -1,8 +1,13 @@
 import React, { useContext, useMemo } from 'react';
 
+import Touchable from '@/components/common/Button/Touchable';
 import RowContainer from '@/components/common/Container/RowContainer';
 import SpaceBetweenContainer from '@/components/common/Container/SpaceBetweenContainer';
-import { BoldPoppins20, NormalPoppins14 } from '@/components/common/Label';
+import {
+    BoldPoppins14,
+    BoldPoppins20,
+    NormalPoppins14,
+} from '@/components/common/Label';
 import RowIconList from '@/components/elements/ExchangeHistory/RowIconList';
 import SummaryBox from '@/components/elements/ExchangingPage/SummaryBox';
 import { ThemeContext } from '@/contexts/ThemeProvider';
@@ -34,6 +39,21 @@ const ExchangeHistoryPage = () => {
                 text={'환전 내역'}
                 style={{ color: colors.LIGHTSHADE900, marginBottom: 24 }}
             />
+            <SpaceBetweenContainer style={listStyle}>
+                <Touchable>
+                    <RowContainer>
+                        <BoldPoppins14
+                            text={'환전시간'}
+                            style={{ color: colors.LIGHTSHADE900 }}
+                        />
+                        <img src={`assets/icons/ArrowDown.svg`} alt={'arrow'} />
+                    </RowContainer>
+                </Touchable>
+                <NormalPoppins14
+                    text={'환전금액'}
+                    style={{ color: colors.LIGHTSHADE900 }}
+                />
+            </SpaceBetweenContainer>
             {[1, 2, 3, 4, 5, 6].map((data) => (
                 <SpaceBetweenContainer style={listStyle}>
                     <NormalPoppins14
